74cf272696d6f0697423a2ac9032b26d9b43c041
rachidmanz <mandounga@gmail.com>
2024-10-03 01:49:01 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
 output "My-public-ip" {
-  value = aws_lightsail_instance.custom.public_ip_address
+  value = aws_lightsail_instance.custom1.public_ip_address
 }
 
 output "My-private-ip" {
-  value = aws_lightsail_instance.custom.private_ip_address
+  value = aws_lightsail_instance.custom1.private_ip_address
 }
 
 output "My_username" {
-  value = aws_lightsail_instance.custom.username
+  value = aws_lightsail_instance.custom1.username
 }
 
 output "My_key_name" {
-  value = aws_lightsail_instance.custom.key_pair_name
+  value = aws_lightsail_instance.custom1.key_pair_name
 }
