2c4677de001b881f693157193ef55b3654257cd9
rachidmanz <mandounga@gmail.com>
2024-10-03 01:45:47 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_lightsail_instance" "custom" {
+resource "aws_lightsail_instance" "custom1" {
   name              = "custom"
   availability_zone = "us-east-1b"
   blueprint_id      = "amazon_linux_2"
